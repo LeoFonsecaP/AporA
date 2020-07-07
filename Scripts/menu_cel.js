8df42aaa2478baead2114e44b722cbf2d8a9e932
LeoFonsecaP <leonardofonsecap@usp.br>
2020-07-07 14:41:48 -0300

    Politica de privacidade feita

--- Scripts/menu_cel.js
+++ Scripts/menu_cel.js
@@ -1,5 +1,3 @@
-const hamburguer = document.getElementsByClassName("ver_mais_cel");
-hamburguer[0].addEventListener("click", function(){
+document.getElementsByClassName("ver_mais_cel")[0].addEventListener("click", function(){
 	document.getElementsByClassName("header_links")[0].classList.toggle("active");
-	hamburguer[0].classList.toggle("active");
 });
